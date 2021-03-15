250ac4f2c11a538478216dda27a52779cabf96ec
Your Name <you@example.com>
2021-03-15 15:29:44 +0200

    add a favicon

--- js/quest-service.js
+++ js/quest-service.js
@@ -24,6 +24,10 @@ function createQuest(txt) {
     }
 }
 
+function randomIntFromInterval(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
 function isChildless(node) {
     return (node.yes === null && node.no === null)
 }
